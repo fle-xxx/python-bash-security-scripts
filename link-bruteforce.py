ab66a19934decb445f9f14c1155bca5c32073189
za-sec <eremeevvanya1@gmail.com>
2020-11-02 14:04:42 +0300

    Update link-bruteforce.py

--- link-bruteforce.py
+++ link-bruteforce.py
@@ -6,18 +6,18 @@ from requests.adapters import HTTPAdapter
 def main():
 
     count = 0
-    root_link = 'https://example.com/index.html?token=AB'
-    adapter = HTTPAdapter(max_retries = 5)
+    root_link = 'https://example.com/index.html?token=AB' # this is a constant part of link
+    adapter = HTTPAdapter(max_retries = 5) # create an adapter, to retry request after unsuccessful attempt
     http = requests.Session()
     http.mount("https://", adapter)
 
     for i in range(100, 200): 
 
-        if keyboard.is_pressed('s'):
+        if keyboard.is_pressed('s'): # bad "Status" button. will be updated 
             print(count)
 
-        final_string = str(i)
-        full_link = root_link + final_string
+        final_string = str(i) # convert variable part of token to string type
+        full_link = root_link + final_string 
         response = http.get(full_link)
 
 
