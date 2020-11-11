c8e8fdb8600b63c41c67127d5d2420816c22a6ad
za-sec <eremeevvanya1@gmail.com>
2020-11-11 03:47:49 +0300

    Update link-bruteforce.sh

--- link-bruteforce.sh
+++ link-bruteforce.sh
@@ -5,5 +5,5 @@ root_link="https://example.com/blabla/"
 for (( i = 500; i < 10000; i++ ))
     do
         url="$root_link$i"
-        curl -I --silent $url | grep 'needed word in response headers' | grep -v 'not needed word in response headers' # silent mode needed for output flexibility
+        curl -I --silent $url | grep 'needed word in response headers' | grep -v 'not needed word in response headers' # silent mode needed for pretty output 
     done
