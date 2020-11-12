f9897a8a2a353ad6bf43f0e361e83787b58f2711
za-sec <eremeevvanya1@gmail.com>
2020-11-12 15:59:22 +0300

    Update link-bruteforce.sh

--- link-bruteforce.sh
+++ link-bruteforce.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
-root_link="https://example.com/blabla/?p="
+root_link="https://example.com/bla/"
+not_needed_string="not_needed_string"
+alphabet="A B C D E F G H I J K L O M N O P Q R S T U V X Y Z a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9"
 
-for (( i = 500; i < 10000; i++ ))
+
+for a in $alphabet
     do
-        url="$root_link$i"
-        curl -I --silent $url | grep 'needed word in response headers' | grep -v 'not needed word in response headers' # silent mode needed for pretty output 
+        for i in $alphabet
+            do
+                for j in $alphabet  
+                    do
+                        for k in $alphabet
+                            do
+                                url="$root_link$a$i$j$k" #creates links like https://example.com/bla/Abc1
+                         
+                                res=$(curl -I --silent $url | grep 'word_we_want_to_catch1.*word_we_want_to_catch2' | grep -v 'word_we_not_want_to_catch1\|word_we_not_want_to_catch2')
+                                
+                                if [[ ("$res" != "") && ("$res" != "$not_needed_string") ]]
+                                    then echo $res; echo -e $url
+                                fi
+                            done   
+                    done
+            done
     done
